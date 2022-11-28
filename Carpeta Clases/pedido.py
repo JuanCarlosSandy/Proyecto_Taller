9effd89e35069905bcb38c4032c64981481cc7cf
JuanCarlosSandy <juan.sandy@uab.edu.bo>
2022-11-27 22:53:17 -0400

    Archivos con la clase que utilizaremos que con (Comida, Admin y Pepidos)

---
+++ Carpeta Clases/pedido.py
@@ -0,0 +1,25 @@
+class Pedido():
+    def __init__(self,idPedido, nombre, comida, cambios, dondeconsume) -> None:
+        self.idPedido = idPedido
+        self.nombre = nombre
+        self.comida = comida
+        self.cambios = cambios
+        self.dondeconsume = dondeconsume
+
+    def getIdPedido(self):
+        return self.idPedido
+
+    def getNombre(self):
+        return self.nombre
+
+    def getComida(self):
+        return self.comida
+
+    def getCambio(self):
+        return self.cambios
+
+    def getDondeconsume(self):
+        return self.dondeconsume
+
+    def __str__(self) -> str:
+        return 'Id: '+str(self.idPedido)+'\nNombre: '+self.nombre+'\nComida: '+'\nNota: '+self.cambios+'\nDonde Consume: '+self.dondeconsume
